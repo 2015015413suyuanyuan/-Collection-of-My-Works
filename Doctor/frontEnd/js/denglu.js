6426f43ad352e401537f973d3c699dc8c64bad9a
2015015413suyuanyuan <743688437@qq.com>
2018-03-08 19:52:37 +0800

    上传口袋医生APP

---
+++ Doctor/frontEnd/js/denglu.js
@@ -0,0 +1,36 @@
+
+  $(".close").click(function(){
+    window.location.href='zhuye.html';
+  });
+
+  $("#zhuce").click(function(){
+   window.location.href='zhuce.html';
+  });
+
+
+  $(".load").click(function(){
+  	$.ajax({
+      url:"http://192.168.163.144:8080/validate_user",
+      dataType:'json',
+      type:'post',
+      data:{userTel:$(".name").val(),userPWD:$('.password').val()},
+      success:function(data){
+        console.log(data);
+        //0 用户名不存在 1  密码不正确 2  密码用户名匹配
+        if(data == 0){
+          $(".name").val('');
+          $('.password').val('');
+          $(".name").attr('placeholder','用户名不存在');
+        }
+        else if(data==1){
+            $('.password').val('');
+            $(".password").attr('placeholder','用户密码不符');
+        }
+        else{
+            localStorage.setItem('username',$(".name").val());
+            console.log(localStorage.getItem('username'));
+           window.location.href='index.html';
+       }
+      }
+    });
+  });
